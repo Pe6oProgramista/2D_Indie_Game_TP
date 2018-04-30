2d409e5353041c1d8a657b75fa469dd3039babc0
pepy3 <p.milev3@gmail.com>
2018-05-01 01:56:16 +0300

    make anchor on right angle and rope start from hand

--- TheGame/TheGame/Assets/Scripts/playerController.cs
+++ TheGame/TheGame/Assets/Scripts/playerController.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     //movement variables
     public float maxSpeed;
@@ -11,6 +12,7 @@ public class playerController : MonoBehaviour {
     //jumping variables
     bool grounded = false;
     float groundCheckRadius = 0.2f;
+    bool canJump = true;
     public LayerMask groundLayer;
     public Transform groundCheck;
     public float jumpHeight;
@@ -18,48 +20,70 @@ public class playerController : MonoBehaviour {
     Rigidbody2D myRB;
     Animator myAnim;
     bool facingRight;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         myRB = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
 
         facingRight = true;
-	}
-	
-	// Update is called once per frame
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (grounded && Input.GetAxis("Jump") > 0)
+        if (Input.GetKey(KeyCode.Space))
         {
-            grounded = false;
-            myAnim.SetBool("isGrounded", grounded);
-            myRB.AddForce(new Vector2(0, jumpHeight));
+            if (grounded)
+            {
+                grounded = false;
+                myAnim.SetBool("isGrounded", grounded);
+                myRB.AddForce(new Vector2(0, jumpHeight));
+                canJump = true;
+            }
+            else if(canJump)
+            {
+                grounded = false;
+                myAnim.SetBool("isGrounded", grounded);
+                myRB.AddForce(new Vector2(0, jumpHeight));
+                canJump = false;
+            }
         }
     }
 
-	void FixedUpdate () {
+    void FixedUpdate()
+    {
 
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         myAnim.SetBool("isGrounded", grounded);
 
         myAnim.SetFloat("verticalSpeed", myRB.velocity.y);
 
-        float move = Input.GetAxis("Horizontal");
+        float move = 0;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            move = 1;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            move = -1;
+        }
+
         myAnim.SetFloat("speed", Mathf.Abs(move));
 
         myRB.velocity = new Vector2(move * maxSpeed, myRB.velocity.y);
 
-        if(move > 0 && !facingRight)
+        if (move > 0 && !facingRight)
         {
-            flip();
+            Flip();
         }
-        else if(move < 0 && facingRight)
+        else if (move < 0 && facingRight)
         {
-            flip();
+            Flip();
         }
-	}
+    }
 
-    void flip()
+    void Flip()
     {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
