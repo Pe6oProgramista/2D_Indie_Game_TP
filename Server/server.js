ea1dc9d6e5faf13c39c21c4b258cae08ed669e1e
pe6oprogramista <p.milev3@gmail.com>
2018-05-30 13:26:39 +0300

    add login logic and take player's last level reached

--- Server/server.js
+++ Server/server.js
@@ -44,18 +44,17 @@ app.post('/register', function(req, res) {
 					return console.error('error running query', err);
 				}
 				
-				var count = console.log(result.rows[0].count);
+				var count = result.rows[0].count;
 				
 				if(count > 0) {
+					console.log('This username or email already exist!');
 					res.send(['Failure', 'This username or email already exist!']);
 				}
 				else {
 					var pool2 = new pg.Pool(config);
-					var authKey = (new tokenGenerator(tokenGenerator.BASE36)).generate();
-					console.log(password);
 					var insertQuery = {
-						text: 'INSERT INTO "Users" ("Username", "Email", "Password", "AuthKey") VALUES ($1, $2, $3, $4)',
-						values: [username, email, password, authKey],
+						text: 'INSERT INTO "Users" ("Username", "Email", "Password") VALUES ($1, $2, $3)',
+						values: [username, email, password],
 					}
 					pool2.connect(function(err, client, done) {
 						if (err) {
@@ -71,7 +70,7 @@ app.post('/register', function(req, res) {
 								console.log("Account added!");
 								done();
 							});
-						res.send(['Success', authKey]);
+						res.send(['Success']);
 					});
 				}
 				done();
@@ -81,16 +80,134 @@ app.post('/register', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-	console.log('loging user: ' + req.body);
+	var data = JSON.parse(req.body['data']),
+		username = data['username'],
+		password = data['password'];
+	console.log('Loging user: ' + username);
+	
+	var query = {
+		text: 'SELECT "Password" FROM "Users" WHERE "Username" = $1',
+		values: [username],
+	}
+	
+	var pool = new pg.Pool(config);
+	
+	pool.connect(function(err, client, done) {
+		if (err) {
+			return console.error('error fetching client from pool', err);
+		}
+	    console.log('Connected to postgres! Getting schemas...');
+
+		client
+			.query(query, function(err, result) {
+				if(err) {
+					return console.error('error running query', err);
+				}
+				
+				var count = result.rowCount;
+				
+				if(count == 0 || password != result.rows[0].Password) {
+					res.send(['Failure', 'Invalid username or password!']);
+				}
+				else {
+					var pool2 = new pg.Pool(config);
+					var authKey = (new tokenGenerator(tokenGenerator.BASE36)).generate();
+					console.log(authKey);
+					var updateQuery = {
+						text: 'UPDATE "Users" SET "AuthKey" = $1 WHERE "Username" = $2',
+						values: [authKey, username],
+					}
+					pool2.connect(function(err, client, done) {
+						if (err) {
+							return console.error('error fetching client from pool', err);
+						}
+						console.log('Connected to postgres! Getting schemas...');
+
+						client
+							.query(updateQuery, function(err, result) {
+								if(err) {
+									return console.error('error running query2', err);
+								}
+								console.log("Loged in!");
+								done();
+							});
+						res.send(['Success', authKey]);
+					});
+				}
+				done();
+			});
+	});
+	pool.end();
 });
 
-app.param('authKey', function(req, res, next, authKey) {
-    req.authKey = authKey;
+app.param('user', function(req, res, next, user) {
+    req.user = user;
     next();
 });
 
-app.get('/leaderboard/:authKey', function(req, res) {
-	console.log('Level number: ' + req.authKey);
+app.get('/levels/:user', function(req, res) {	
+	console.log('Showing $1\'s levels...' + req.user);
+	
+	var query = {
+		text: 'SELECT COUNT(1) FROM "Users" WHERE "AuthKey" = $1',
+		values: [req.user],
+	}
+	
+	var pool = new pg.Pool(config);
+	
+	pool.connect(function(err, client, done) {
+		if (err) {
+			return console.error('error fetching client from pool', err);
+		}
+	    console.log('Connected to postgres! Getting schemas...');
+
+		client
+			.query(query, function(err, result) {
+				if(err) {
+					return console.error('error running query', err);
+				}
+				
+				var count = result.rowCount;
+				if(count == 0) {
+					res.send(['Failure', 'Connection error!']);
+				}
+				else {
+					var pool2 = new pg.Pool(config);
+					var getQuery = {
+						text: 'SELECT "LastLevel" FROM "Users" WHERE "AuthKey" = $1',
+						values: [req.user],
+					}
+					pool2.connect(function(err, client, done) {
+						if (err) {
+							return console.error('error fetching client from pool', err);
+						}
+						console.log('Connected to postgres! Getting schemas...');
+
+						client
+							.query(getQuery, function(err, result) {
+								if(err) {
+									return console.error('error running query2', err);
+								}
+								
+								res.send(['Success', result.rows[0].LastLevel]);
+								done();
+							});
+					});
+				}
+				
+				done();
+			});
+	});
+	pool.end();
+});
+
+app.param('levelNumber', function(req, res, next, levelNumber) {
+    req.levelNumber = levelNumber;
+    next();
+});
+
+app.get('/leaderboard/:levelNumber', function(req, res) {
+	console.log('Level number: ' + req.levelNumber);
 });
 
 app.listen(port, function() {
