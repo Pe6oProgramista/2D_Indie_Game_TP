261d0cd267f861fa608bff0529a80b5438d579e8
pe6oprogramista <p.milev3@gmail.com>
2018-05-31 02:07:39 +0300

    make levels sprites to be readable and writeable

--- TheGame/TheGame/Assets/AudioControler.cs
+++ TheGame/TheGame/Assets/AudioControler.cs
@@ -5,9 +5,31 @@ using UnityEngine;
 public class AudioControler : MonoBehaviour {
 
     public AudioClip[] audios;
+    AudioSource audioSource;
 
-	void Start () {
+    int currClipIndex = 0;
+
+    void Start()
+    {
         DontDestroyOnLoad(gameObject);
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audios[currClipIndex];
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            if(currClipIndex + 1 == audios.Length)
+            {
+                currClipIndex = 0;
+            }
+            else
+            {
+                currClipIndex++;
+            }
+            audioSource.clip = audios[currClipIndex];
+            audioSource.Play();
+        }
     }
 }
